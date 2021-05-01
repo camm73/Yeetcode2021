2e2871e9aa94301786e0757079091194f8908048
Cameron Miller <camm73@yahoo.com>
2021-05-01 14:52:44 -0700

    Comment out test code

--- spotify_smash.py
+++ spotify_smash.py
@@ -62,12 +62,14 @@ def rank_songs(songs_features: dict = {}, rank_categories: list = []) -> dict:
                 heappush(category_heap[category], category_tuple)
     return category_heap
 
+'''
 token = ""
 rank_categories = ["danceability", "energy", "acousticness"]
 top_songs = get_users_top_songs([token], 5)
 audio_features = get_audio_features([token], top_songs)
 heap = rank_songs(audio_features, rank_categories)
 print(heap)
+'''
 
 # Gets top songs from each category
 # Returns a list of songIDs for the final playlist
