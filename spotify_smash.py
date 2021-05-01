ea95bb5290cb9df1a92219fb23a3c9fd2ca20373
Cameron Miller <camm73@yahoo.com>
2021-05-01 16:43:58 -0700

    Bug fixes; more status messages

--- spotify_smash.py
+++ spotify_smash.py
@@ -2,11 +2,13 @@ import requests
 import datetime
 from heapq import heappop, heappush
 import sys
+import json
 
 # Get the top "song_count" songs from every user passed in
 # Returns a dictionary from songID ("key") to another dictionary with keys ("total_users", "uri", "song_data")
 def get_users_top_songs(user_tokens: list = [], song_count: int = 100) -> dict:
     song_dict = {}
+    print("Getting top songs for all users...")
     for token in user_tokens:
         # Make request for user's top songs
         response = requests.get(
@@ -33,12 +35,14 @@ def get_users_top_songs(user_tokens: list = [], song_count: int = 100) -> dict:
                     "uri": song_uri,
                     "song_data": single_song
                 }
+    print("Successfully got top songs for all users")
     return song_dict
 
 # Get audio features for each song in the dictionary
 # Returns a dictionary from songID ("key") to another dictionary with keys ("total_users", "uri", "song_data", "audio_features")
 def get_audio_features(user_tokens: list = [], song_dict: dict = {}) -> dict:
     song_features = song_dict
+    print("Getting audio features for all songs...")
     for song_id in song_dict:
         response = requests.get(
             f"https://api.spotify.com/v1/audio-features/{song_id}",
@@ -53,6 +57,7 @@ def get_audio_features(user_tokens: list = [], song_dict: dict = {}) -> dict:
 
         song_response = response.json()
         song_features[song_id]["audio_features"] = song_response
+    print("Successfully got audio features for all songs...")
     return song_features
 
 # Rank songs by certain features
@@ -60,6 +65,7 @@ def get_audio_features(user_tokens: list = [], song_dict: dict = {}) -> dict:
 # Each heap is indexed in a dict by category name
 def rank_songs(songs_features: dict = {}, rank_categories: list = []) -> dict:
     category_heap = {}
+    print("Starting to rank songs by category")
     for song_id in songs_features:
         for category in rank_categories:
             song_uri = songs_features[song_id]['uri']
@@ -70,15 +76,9 @@ def rank_songs(songs_features: dict = {}, rank_categories: list = []) -> dict:
             else:
                 category_heap[category] = []
                 heappush(category_heap[category], category_tuple)
+    print("Successfully ranked all songs")
     return category_heap
 
-
-token = ""
-rank_categories = ["danceability", "energy", "acousticness"]
-top_songs = get_users_top_songs([token], 5)
-audio_features = get_audio_features([token], top_songs)
-heap = rank_songs(audio_features, rank_categories)
-print(heap)
 
 # Gets top songs from each category
 # Returns a list of songURIs for the final playlist
@@ -86,28 +86,50 @@ def get_top_category_songs(ranked_songs: dict = {}, total_songs: int = 100) -> l
     pass
 
 
+# Get the user profile and return the Spotify user ID
+def get_user_id(token: str = "") -> str:
+    user_res = requests.get(
+        "https://api.spotify.com/v1/me",
+        headers={
+            "Authorization": f"Bearer {token}"
+        }
+    )
+
+    if user_res.status_code != 200:
+        print(f"Failed to get user profile: {user_res.json()}")
+        sys.exit(1)
+    
+    user_id = user_res.json()['id']
+    return user_id
+
+
+
 # Make the final spotify playlist from the songs
 # Returns identifier of the new playlist
-def make_final_playlist(user_tokens: list = [], final_songs: list = [], user_id: str = "") -> str:
+def make_final_playlist(user_tokens: list = [], final_songs: list = []) -> str:
+    user_id = get_user_id(user_tokens[0])
+    print(f"Making a new playlist for user {user_id}...")
     playlist_response = requests.post(
         f"https://api.spotify.com/v1/users/{user_id}/playlists",
         headers={
             "Authorization": f"Bearer {user_tokens[0]}"
         },
-        data = {
+        data=json.dumps({
             "name": f"Spotify Smash {datetime.datetime.now().isoformat()}",
             "description": "A SUPER SMASHIN PLAYLIST",
-            "public": True,
-            "collaborative": True
-        }
+            "public": "false",
+            "collaborative": "true"
+        })
     )
-    if playlist_response.status_code != 200:
+    if playlist_response.status_code != 200 and playlist_response.status_code != 201:
         print(f"Failed to make playlist: {playlist_response.json()}")
         sys.exit(1)
 
     playlist_id = playlist_response.json()["id"]
+    print(f"Made playlist with ID: {playlist_id}")
     
     curr_song = 0
+    print("Adding all songs to playlists...")
     while curr_song < len(final_songs):
         end_index = min(curr_song+99, len(final_songs))
         add_songs_res = requests.post(
@@ -115,20 +137,23 @@ def make_final_playlist(user_tokens: list = [], final_songs: list = [], user_id:
             headers={
                 "Authorization": f"Bearer {user_tokens[0]}"
             },
-            data = {
+            data=json.dumps({
                 "uris": final_songs[curr_song:end_index]
-            }
+            })
         )
-        if add_songs_res.status_code != 200:
+        if add_songs_res.status_code != 200 and add_songs_res.status_code != 201:
             print(f"Failed to add songs to playlist: {add_songs_res.json()}")
             sys.exit(1)
 
         curr_song = end_index
+    
+    print("Successfully added all songs to playlist!")
     return playlist_id
 
 
 # Add playlist to each user's account
 def add_playlist_to_accounts(user_tokens: list = [], playlist_id: str = '') -> None:
+    print("Adding playlists to user accounts")
     for i in range(1, len(user_tokens)):
         add_playlist_res = requests.put(
             f"https://api.spotify.com/v1/playlists/{playlist_id}/followers",
@@ -139,7 +164,23 @@ def add_playlist_to_accounts(user_tokens: list = [], playlist_id: str = '') -> N
         
         if add_playlist_res.status_code != 200:
             print(f"Failed to add playlist to user ({user_tokens[i]}): {add_playlist_res.json()}")
+    print("Successfully added playlists to user accounts")
 
+
+'''
+token = ""
+rank_categories = ["danceability", "energy", "acousticness"]
+top_songs = get_users_top_songs([token], 5)
+audio_features = get_audio_features([token], top_songs)
+heap = rank_songs(audio_features, rank_categories)
+
+song_list = []
+for song in heap['energy']:
+    my_uri = song[2]
+    song_list.append(my_uri)
+
+make_final_playlist(user_tokens=[token], final_songs=song_list)
+'''
 
 # Entry code and argument handling
 if __name__ == "__main__":
